e13d8105f3865dfb19bb84cd5ce7b033be9860cd
ionutpetrovici <ionutpetrovici200@gmail.com>
2023-07-19 19:01:20 +0300

    exercitii extra 2

--- index.js
+++ index.js
@@ -75,6 +75,44 @@ console.log("Astazi este " + days);
 
 // EXERCITII EXTRA
 
+
+// 1. Define the expected oven time in minutes
+// Define the EXPECTED_MINUTES_IN_OVEN constant that represents how many minutes the lasagna should be in the oven. It must be exported. According to the cooking book, the expected oven time in minutes is 40.
+
+const EXPECTED_MINUTES_IN_OVEN = 40;
+
+
+// 2. Calculate the remaining oven time in minutes
+// Implement the remainingMinutesInOven function that takes the actual minutes the lasagna has been in the oven as a parameter and returns how many minutes the lasagna still has to remain in the oven, based on the expected oven time in minutes from the previous task.
+
+function remainingMinutesInOven(actualMinutes) {
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutes;
+};
+
+
+// 3. Calculate the preparation time in minutes
+// Implement the preparationTimeInMinutes function that takes the number of layers you added to the lasagna as a parameter and returns how many minutes you spent preparing the lasagna, assuming each layer takes you 2 minutes to prepare.
+
+function preparationTimeInMinutes(numberOfLayers) {
+  return numberOfLayers * 2;
+};
+
+
+// 4. Calculate the total working time in minutes
+// Implement the totalTimeInMinutes function that takes two parameters: the numberOfLayers parameter is the number of layers you added to the lasagna, and the actualMinutesInOven parameter is the number of minutes the lasagna has been in the oven. The function should return how many minutes in total you've worked on cooking the lasagna, which is the sum of the preparation time in minutes, and the time in minutes the lasagna has spent in the oven at the moment.
+
+function totalTimeInMinutes(numberOfLayers, actualMinutes) {
+  const preparationTime = preparationTimeInMinutes(numberOfLayers);
+  return preparationTime + actualMinutes;
+};
+
+console.log(remainingMinutesInOven(30));
+console.log(preparationTimeInMinutes(2));
+console.log(totalTimeInMinutes(2, 30));
+
+// --------------------- //
+
+
 // 1. Check if the 'Fast Attack' action is possible
 // Implement a function named canExecuteFastAttack that takes a boolean value which indicates if the knight is awake. This function returns true if the 'Fast Attack' action is available based on the state of the character. Otherwise, returns false:
 
