c1231d37bf01d5dbce9face3eb4ed90ceb5908f8
ionutpetrovici <ionutpetrovici200@gmail.com>
2023-07-19 11:34:23 +0300

    exercitii bonus

--- index.js
+++ index.js
@@ -71,3 +71,69 @@ switch (day) {
 }
 
 console.log("Astazi este " + days);
+
+
+// EXERCITII EXTRA
+
+// 1. Check if the 'Fast Attack' action is possible
+// Implement a function named canExecuteFastAttack that takes a boolean value which indicates if the knight is awake. This function returns true if the 'Fast Attack' action is available based on the state of the character. Otherwise, returns false:
+
+const knightIsAwake = false;
+const archerIsAwake = true;
+const prisonerIsAwake = false;
+const petDogIsPresent = true;
+
+let canexecutFastAttack = (sleeping) => {
+  if (sleeping === false) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log("Can execut fast attack: - " + canexecutFastAttack(knightIsAwake));
+
+// 2. Check if the 'Spy' action is possible
+// Implement a function named canSpy that takes three boolean values, indicating if the knight, archer and the prisoner, respectively, are awake. The function returns true if the 'Spy' action is available based on the state of the characters. Otherwise, returns false:
+
+let canSpy = (knightState, archerState, prisonerState) => {
+  if (knightState && archerState && prisonerState === true) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log("Can Spy: - " + canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake));
+
+
+// 3. Check if the 'Signal Prisoner' action is possible
+// Implement a function named canSignalPrisoner that takes two boolean values, indicating if the archer and the prisoner, respectively, are awake. The function returns true if the 'Signal Prisoner' action is available based on the state of the characters. Otherwise, returns false:
+
+let canSignalPrisoner = (archerIsAwake, prisonerIsAwake) => {
+  if (archerIsAwake || prisonerIsAwake === true) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log("Can signal the prisoner: - " + canSignalPrisoner(archerIsAwake, prisonerIsAwake));
+
+
+// 4. Check if the 'Free Prisoner' action is possible
+// Implement a function named canFreePrisoner that takes four boolean values. The first three parameters indicate if the knight, archer and the prisoner, respectively, are awake. The last parameter indicates if Annalyn's pet dog is present. The function returns true if the 'Free Prisoner' action is available based on the state of the characters and Annalyn's pet dog presence. Otherwise, it returns false:
+
+let canFreePrisoner = (knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent) => {
+  if (knightIsAwake || archerIsAwake || prisonerIsAwake === true) {
+    return false;
+  }
+  else if (petDogIsPresent === true) {
+    return true;
+  }
+}
+
+console.log("Can free the prisoner: - " + canFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent));
